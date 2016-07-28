099363e09bdbf6f71c63f8711a1f6a1fa0b7943a
Ben Knight <benjamin.r.knight@vanderbilt.edu>
2016-07-28 16:02:19 -0500

    Change Migration Method

--- db/migrate/20160630193050_create_users.rb
+++ db/migrate/20160630193050_create_users.rb
@@ -7,6 +7,10 @@ class CreateUsers < ActiveRecord::Migration
       t.string :carry
       t.string :deviceType
       t.string :searchedguest
+      t.string :followercount
+      t.string :profilelink
+      t.string :bio
+      t.string :website
       t.belongs_to :guest
       t.timestamps null: false
     end
--- db/migrate/20160728174220_add_data_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddDataToUsers < ActiveRecord::Migration
-  def change
-  	add_column :users, :followercount, :string
-  	add_column :users, :profilelink, :string
-  	add_column :users, :bio, :string
-  	add_column :users, :website, :string
-  end 
-end
