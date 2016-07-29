f3424d90c7aa52610f8fc1bd7eb956adc45ad7ff
Ben Knight <benjamin.r.knight@vanderbilt.edu>
2016-07-29 16:42:05 -0500

    Add to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
 
   config.secret_key_base = ENV["SECRET_KEY_BASE"]
-  
+  config.serve_static_files = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
